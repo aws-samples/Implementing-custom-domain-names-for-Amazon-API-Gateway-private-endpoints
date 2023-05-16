8fdea27953ad4a293924e9583413c272fa7026a5
Jason Enderle <jasonend@amazon.com>
2023-05-16 11:35:26 -0400

    Update nginx config for apigw header.

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,64 +1,64 @@
-# resource "local_file" "proxy-config" {
-#   filename = "${path.module}/..//${var.app_name}-${var.app_environment}-proxy-conf.yaml"
-#   content  = templatefile("${path.module}/template_files/proxy-config.tftpl", yamldecode(file(local.proxy_config_path)))
-# }
+resource "local_file" "proxy-config" {
+  filename = "${path.module}/..//${var.app_name}-${var.app_environment}-proxy-conf.yaml"
+  content  = templatefile("${path.module}/template_files/proxy-config.tftpl", yamldecode(file(local.proxy_config_path)))
+}
 
-# resource "local_file" "output" {
-#   filename = "${path.module}/../outputs/outputs.json"
-#   content = jsonencode({
-#     vpc_id              = local.vpc_id
-#     api_gateway_vpce_id = module.endpoints.endpoints["execute-api"].id
-#     elb_dns_uri         = module.load_balancer.lb_dns_name
-#     mapping = [for api in local.api_list : {
-#       CUSTOM_DOMAIN_URL       = api.CUSTOM_DOMAIN_URL
-#       PRIVATE_API_URL         = api.PRIVATE_API_URL
-#       VERBS                   = can(api.VERBS) ? api.VERBS : ["*"]
-#       PRIVATE_RESOURCE_POLICY = [jsondecode(data.aws_iam_policy_document.this[api.CUSTOM_DOMAIN_URL].json)]
-#     }]
-#   })
-# }
+resource "local_file" "output" {
+  filename = "${path.module}/../outputs/outputs.json"
+  content = jsonencode({
+    vpc_id              = local.vpc_id
+    api_gateway_vpce_id = module.endpoints.endpoints["execute-api"].id
+    elb_dns_uri         = module.load_balancer.lb_dns_name
+    mapping = [for api in local.api_list : {
+      CUSTOM_DOMAIN_URL       = api.CUSTOM_DOMAIN_URL
+      PRIVATE_API_URL         = api.PRIVATE_API_URL
+      VERBS                   = can(api.VERBS) ? api.VERBS : ["*"]
+      PRIVATE_RESOURCE_POLICY = [jsondecode(data.aws_iam_policy_document.this[api.CUSTOM_DOMAIN_URL].json)]
+    }]
+  })
+}
 
-# data "aws_iam_policy_document" "this" {
-#   for_each = { for api in local.api_list : api.CUSTOM_DOMAIN_URL => {
-#     api_stage     = split("/", api.PRIVATE_API_URL)[3],
-#     resource_path = can(split(regex("(https://(.*/){2})", api.PRIVATE_API_URL), api.PRIVATE_API_URL)[1]) ? split(regex("(https://(.*/){2})", api.PRIVATE_API_URL), api.PRIVATE_API_URL)[1] : "*",
-#     verbs         = can(api.VERBS) ? api.VERBS : ["*"]
-#     vpce          = module.endpoints.endpoints["execute-api"].id,
-#     }
-#   }
-#   statement {
-#     sid    = "${var.app_name}-${var.app_environment}-allow"
-#     effect = "Allow"
-#     principals {
-#       type        = "*"
-#       identifiers = ["*"]
-#     }
-#     actions = [
-#       "execute-api:Invoke"
-#     ]
-#     resources = [for verb in each.value.verbs :
-#       "execute-api:/${each.value.api_stage}/${verb}/${each.value.resource_path}"
-#     ]
-#   }
-#   statement {
-#     sid    = "${var.app_name}-${var.app_environment}-deny"
-#     effect = "Deny"
-#     principals {
-#       type        = "*"
-#       identifiers = ["*"]
-#     }
-#     actions = [
-#       "execute-api:Invoke"
-#     ]
-#     resources = [for verb in each.value.verbs :
-#       "execute-api:/${each.value.api_stage}/${verb}/${each.value.resource_path}"
-#     ]
-#     condition {
-#       test     = "StringNotEquals"
-#       variable = "aws:SourceVpce"
-#       values = [
-#         each.value.vpce
-#       ]
-#     }
-#   }
-# }
+data "aws_iam_policy_document" "this" {
+  for_each = { for api in local.api_list : api.CUSTOM_DOMAIN_URL => {
+    api_stage     = split("/", api.PRIVATE_API_URL)[3],
+    resource_path = can(split(regex("(https://(.*/){2})", api.PRIVATE_API_URL), api.PRIVATE_API_URL)[1]) ? split(regex("(https://(.*/){2})", api.PRIVATE_API_URL), api.PRIVATE_API_URL)[1] : "*",
+    verbs         = can(api.VERBS) ? api.VERBS : ["*"]
+    vpce          = module.endpoints.endpoints["execute-api"].id,
+    }
+  }
+  statement {
+    sid    = "${var.app_name}-${var.app_environment}-allow"
+    effect = "Allow"
+    principals {
+      type        = "*"
+      identifiers = ["*"]
+    }
+    actions = [
+      "execute-api:Invoke"
+    ]
+    resources = [for verb in each.value.verbs :
+      "execute-api:/${each.value.api_stage}/${verb}/${each.value.resource_path}"
+    ]
+  }
+  statement {
+    sid    = "${var.app_name}-${var.app_environment}-deny"
+    effect = "Deny"
+    principals {
+      type        = "*"
+      identifiers = ["*"]
+    }
+    actions = [
+      "execute-api:Invoke"
+    ]
+    resources = [for verb in each.value.verbs :
+      "execute-api:/${each.value.api_stage}/${verb}/${each.value.resource_path}"
+    ]
+    condition {
+      test     = "StringNotEquals"
+      variable = "aws:SourceVpce"
+      values = [
+        each.value.vpce
+      ]
+    }
+  }
+}
